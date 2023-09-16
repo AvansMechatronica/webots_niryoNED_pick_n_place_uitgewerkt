e809add5af94508e61a4d9070cf8ab03fe5050e7
GerardHarkemaAvans <44970086+GerardHarkemaAvans@users.noreply.github.com>
2023-09-16 12:24:35 +0200

    '20230916'

--- controllers/conveyor_belt_controller/conveyor_belt_controller.py
+++ controllers/conveyor_belt_controller/conveyor_belt_controller.py
@@ -1,19 +1,31 @@
 """conveyor_belt_controller controller."""
 
 # You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
-from controller import Robot
+#  from controller import conveyor_belt, Motor, Distancedistance_sensor
+#from controller import conveyor_belt
+from controller import Supervisor
+import random
+# create the conveyor_belt instance.
+supervisor = Supervisor()
+conveyor_belt = supervisor
 
-# create the Robot instance.
-robot = Robot()
+
+#set distance distance_sensor to random position
+distance_sensor_node = supervisor.getFromDef("DIST_SENSOR")
+trans_field = distance_sensor_node.getField("translation")
+distance_sensor_position = trans_field.getSFVec3f()
+# calculate new translation
+distance_sensor_position[0] = 0.50 + (random.random() * 0.2)
+trans_field.setSFVec3f(distance_sensor_position)
+distance_sensor_node.resetPhysics()
 
 # get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+timestep = int(conveyor_belt.getBasicTimeStep())
 
 # You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-belt = robot.getDevice('belt_motor')
-ds = robot.getDevice('Distance Sensor')
+# instance of a device of the conveyor_belt. Something like:
+belt = conveyor_belt.getDevice('belt_motor')
+ds = conveyor_belt.getDevice('Distance Sensor')
 ds.enable(timestep)
 
 belt.setVelocity(0.1)
@@ -21,21 +33,15 @@ belt.setPosition(float('inf'))
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
+while conveyor_belt.step(timestep) != -1:
+    # Read the distance_sensors:
+    # Enter here functions to read distance_sensor data, like:
     val = ds.getValue()
-    print(val)
+    #print(val)
 
     if val > 1.0:
         belt.setVelocity(0)
     
-
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
     pass
 
 # Enter here exit cleanup code.
